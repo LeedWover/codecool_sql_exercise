0b922a6cbe08198ef0027022003838d9987e7d11
Nagy Gábor <aresone.gaben@gmail.com>
2021-11-24 16:08:09 +0100

    tables created

--- create_projects_table.sql
+++ create_projects_table.sql
@@ -1,7 +1,14 @@
+CREATE TYPE proj_type AS ENUM ('exercise', 'tutorial');
+
 CREATE TABLE projects (
     id SERIAL PRIMARY KEY,
     student_id INT,
-    CONSTRAINT fk_student_id FOREIGN KEY (student_id) REFERENCES Students(id),
-    project_name: VARCHAR(50),
-    
+    CONSTRAINT fk_student_id FOREIGN KEY (student_id) REFERENCES students(id),
+    course_id INT,
+    CONSTRAINT fk_course_id FOREIGN KEY (course_id) REFERENCES courses(id),
+    "name" VARCHAR(50),
+    description VARCHAR(255),
+    materials VARCHAR(255),
+    is_team_project BOOLEAN,
+    "type" proj_type
 )
